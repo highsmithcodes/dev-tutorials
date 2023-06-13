0038adf30ad9daa77fb2ac9db183126d475a6cf5
Highsmith Codes <ezrahighsmith@gmail.com>
2023-06-13 19:46:33 +0000

    add an absolute path to appi

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors package
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
-const posts = require('./db-api.json');
+const posts = require(path.join(__dirname, 'db-api.json'));
 
 const corsOptions = {
-    origin: 'https://www.devweight.com', // Replace '*' with the appropriate domain of your React application
-    methods: 'GET',
-    allowedHeaders: 'Content-Type',
-  };
-  
+  origin: 'https://www.devweight.com',
+  methods: 'GET',
+  allowedHeaders: 'Content-Type',
+};
+
 app.use(cors(corsOptions));
-// app.use(cors()); // Enable CORS for all routes
+
 app.get('/', (req, res) => {
-    res.send('API is running');
-  });
+  res.send('API is running');
+});
 
 app.get('/api/posts', (req, res) => {
-    console.log(posts)
+  console.log(posts);
   res.json(posts);
 });
 
