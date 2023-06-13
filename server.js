93cd544d6b5a388562724b4f07b4f9eba0d6c6e5
Highsmith Codes <ezrahighsmith@gmail.com>
2023-06-13 18:33:19 +0000

    update server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const app = express();
 const posts = require('./db-api.json');
+
+app.use(cors()); // Enable CORS for all routes
 
 app.get('/api/posts', (req, res) => {
   res.json(posts);
